3b07ff9cf6aa0d26ba3b69c0c303f6821396db9c
Hergy Tchuinkou <htc@adorsys.de>
2020-04-23 19:22:20 +0200

    deployment and persistence

--- book-library-ui/src/app/common/services/interceptor.ts
+++ book-library-ui/src/app/common/services/interceptor.ts
@@ -1,2 +1,17 @@
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Injectable} from "@angular/core";
 
+@Injectable()
+export class ApiInterceptor implements HttpInterceptor {
 
+    constructor() {
+    }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        req = req.clone({
+            withCredentials: true});
+
+        return next.handle(req);
+    }
+}
--- cmd/book-api/main.go
+++ cmd/book-api/main.go
@@ -16,14 +16,15 @@ import (
 	"contrib.go.opencensus.io/exporter/zipkin"
 	"github.com/ardanlabs/conf"
 	"github.com/book-library/cmd/book-api/internal/handlers"
+	_ "github.com/book-library/cmd/book-api/internal/swaggerui"
 	"github.com/book-library/internal/platform/auth"
 	"github.com/book-library/internal/platform/database"
 	"github.com/dgrijalva/jwt-go"
 	openzipkin "github.com/openzipkin/zipkin-go"
 	zipkinHTTP "github.com/openzipkin/zipkin-go/reporter/http"
 	"github.com/pkg/errors"
-	"go.opencensus.io/trace"
 	_ "github.com/rakyll/statik/fs"
+	"go.opencensus.io/trace"
 )
 
 // build is the git version of this program. It is set using build flags in the makefile.
@@ -63,9 +64,9 @@ func run() error {
 		}
 		Auth struct {
 			KeyID          string `conf:"default:1"`
-//			PrivateKeyFile string `conf:"default:/app-library/private.pem"`
-			PrivateKeyFile string `conf:"default:private.pem"`
-			Algorithm      string `conf:"default:RS256"`
+			PrivateKeyFile string `conf:"default:/app-library/private.pem"`
+			//			PrivateKeyFile string `conf:"default:private.pem"`
+			Algorithm string `conf:"default:RS256"`
 		}
 		Zipkin struct {
 			LocalEndpoint string  `conf:"default:0.0.0.0:3000"`
