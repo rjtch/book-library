59086513858433b38d7f695b41ca3ffc1235fcba
rjtch <tchuinkoufongue@gmail.com>
2024-06-15 15:03:15 +0200

    make github-action work
    Signed-off-by: rjtch <tchuinkoufongue@gmail.com>

--- book-library-client/book-library-ui/api/api/api.ts
+++
@@ -1,9 +0,0 @@
-export * from './books.service';
-import { BooksService } from './books.service';
-export * from './categories.service';
-import { CategoriesService } from './categories.service';
-export * from './loans.service';
-import { LoansService } from './loans.service';
-export * from './users.service';
-import { UsersService } from './users.service';
-export const APIS = [BooksService, CategoriesService, LoansService, UsersService];
